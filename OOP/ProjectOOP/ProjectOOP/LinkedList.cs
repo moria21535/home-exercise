0ff494e281ed4a6e746a54594fd97555a8d106d0
moria21535 <moriah3443@gmail.com>
2024-05-14 02:06:52 +0300

    Fixed : GetMaxNode, GetMinNode

--- OOP/ProjectOOP/ProjectOOP/LinkedList.cs
+++ OOP/ProjectOOP/ProjectOOP/LinkedList.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ProjectOOP
 {
@@ -10,15 +6,15 @@ namespace ProjectOOP
     {
         public Node First { get; private set; }
         public Node Last { get; set; }
-        private List<Node> max;
-        private List<Node> min;
+        private Node max;
+        private Node min;
 
         public LinkedList()
         {
             First = null;
             Last = First;
-            max = new List<Node>();
-            min = new List<Node>();
+            max = null;
+            min = null;
         }
 
         public void Append(int value)
@@ -29,8 +25,8 @@ namespace ProjectOOP
                 Last = new Node(value);
                 First = Last;
 
-                max.Add(Last);
-                min.Add(Last);
+                max = Last;
+                min = Last;
             }
 
             else
@@ -38,20 +34,16 @@ namespace ProjectOOP
                 Last.Next = new Node(value);
                 Last = Last.Next;
 
-                int maxLastIndex = max.Count - 1;
-                int minLastIndex = min.Count - 1;
-                if (value >= max[maxLastIndex].Value)
+                if (value > max.Value)
                 {
-                    max.Add(Last);
+                    max = Last;
                 }
 
-                if (value <= min[minLastIndex].Value)
+                else if (value < min.Value)
                 {
-                    min.Add(Last);
+                    min = Last;
                 }
-
             }
-
         }
 
         public void Prepend(int value)
@@ -62,42 +54,26 @@ namespace ProjectOOP
             {
                 Last = First;
 
-                max.Add(First);
-                min.Add(First);
+                max = First;
+                min = First;
             }
 
             else
             {
-                int maxLastIndex = max.Count - 1;
-                int minLastIndex = min.Count - 1;
-                if (value >= max[maxLastIndex].Value)
+                if (value > max.Value)
                 {
-                    max.Add(First);
+                    max = First;
                 }
 
-                if (value <= min[minLastIndex].Value)
+                else if (value < min.Value)
                 {
-                    min.Add(First);
+                    min = First;
                 }
-            }
-
-            
+            }  
         }
 
         public int Pop()
         {
-            int maxLastIndex = max.Count - 1;
-            int minLastIndex = min.Count - 1;
-            if ((max.Count != 0) && (max[maxLastIndex] == Last))
-            {
-                max.RemoveAt(maxLastIndex);
-            } 
-            
-            if ((min.Count != 0) && (min[minLastIndex] == Last))
-            {
-                min.RemoveAt(minLastIndex);
-            }
-
             Node preLast = First;
             int value = -1;
 
@@ -112,8 +88,11 @@ namespace ProjectOOP
                 {
                     value = Last.Value;
                     First = null;
-                    // First = First.Next;
                     Last = First;
+
+                    max = First;
+                    min = First;
+
                     return value;
                 }
             }
@@ -123,29 +102,25 @@ namespace ProjectOOP
                 preLast = preLast.Next;
             }
 
-
             value = Last.Value;
             preLast.Next = null;
-            // preLast.Next = Last.Next;
+
+            if (max == Last)
+            {
+                FindMax();
+            }
+
+            else if (min == Last)
+            {
+                FindMin();
+            }
+
             Last = preLast;
             return value;
-
         }
 
         public int Unqueue()
         {
-            int maxLastIndex = max.Count - 1;
-            int minLastIndex = min.Count - 1;
-            if ((max.Count != 0) && (max[maxLastIndex] == First))
-            {
-                max.RemoveAt(maxLastIndex);
-            }
-
-            if ((min.Count != 0) && (min[minLastIndex] == First))
-            {
-                min.RemoveAt(minLastIndex);
-            }
-
             int value = -1;
 
             if (First == Last)
@@ -159,15 +134,29 @@ namespace ProjectOOP
                 {
                     value = First.Value;
                     First = null;
-                    // First = First.Next;
                     Last = First;
+
+                    min = First;
+                    max = First;
+
                     return value;
                 }
-
             }
 
             value = First.Value;
+            Node removeNode = First;
             First = First.Next;
+
+            if (max == removeNode)
+            {
+                FindMax();
+            }
+
+            else if (min == removeNode)
+            {
+                FindMin();
+            }
+
             return value;
         }
 
@@ -200,17 +189,6 @@ namespace ProjectOOP
             pos.Next = newnode;
             pos = newnode;
             return pos;
-
-            //if (pos == null)
-            //{
-            //    Prepend(val);
-            //    pos = First;
-            //    return pos;
-            //}
-
-            //Append(val);
-            //pos = Last;
-            //return pos;
         }
 
         private void InsertIntoPartialSortedList(Node untilPos)
@@ -248,23 +226,10 @@ namespace ProjectOOP
             pos.Next = null;
             pos = prev.Next;
             return pos;
-
-            //if (pos == First)
-            //{
-            //    Unqueue();
-            //    pos = First;
-            //    return pos;
-            //}
-
-            //Pop();
-            //pos = Last.Next;
-            //return pos;
-
         }
 
         public void Sort()
         {
-
             Node untilPos = First;
             while (untilPos != null)
             {
@@ -281,26 +246,47 @@ namespace ProjectOOP
             Last = p;
         }
 
+        private void FindMax()
+        {
+            Node p = First.Next;
+            max = First;
+
+            while (p != null)
+            {
+                if (p.Value > max.Value)
+                {
+                    max = p;
+                }
+
+                p = p.Next;
+            }
+        }
+
+        private void FindMin()
+        {
+            Node p = First.Next;
+            min = First;
+
+            while (p != null)
+            {
+                if (p.Value < min.Value)
+                {
+                    min = p;
+                }
+
+                p = p.Next;
+            }
+        }
+
         public Node GetMaxNode()
         {
-            if (max.Count != 0)
-            {
-                return max[max.Count - 1];
-            }
-
-            return null;
+            return max;
         }
 
         public Node GetMinNode()
         {
-            if (min.Count != 0)
-            {
-                return min[min.Count - 1];
-            }
-
-            return null;
+            return min;
         }
     }
-            
-                   
+                 
 }
--- OOP/ProjectOOP/ProjectOOP/Program.cs
+++ OOP/ProjectOOP/ProjectOOP/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace ProjectOOP
 {
@@ -7,32 +6,23 @@ namespace ProjectOOP
     {
         static void Main(string[] args)
         {
-            //LinkedList lst = new LinkedList();
-            //lst.Append(3);
-            //lst.Append(2);
-            //lst.Append(1);
-            //Console.WriteLine(string.Join(" ", lst.ToList()));
-            //Console.WriteLine($"first: {lst.First.Value}");
-            //Console.WriteLine($"last: {lst.Last.Value}");
+            LinkedList lst = new();
+            lst.Append(3);
+            lst.Append(2);
+            lst.Append(1);
+            Console.WriteLine(string.Join(" ", lst.ToList()));
+            Console.WriteLine($"first: {lst.First.Value}");
+            Console.WriteLine($"last: {lst.Last.Value}");
 
-            //Console.WriteLine();
-            //lst.Sort();
-            //Console.WriteLine(string.Join(" ", lst.ToList()));
+            Console.WriteLine();
+            lst.Sort();
+            Console.WriteLine(string.Join(" ", lst.ToList()));
+            Console.WriteLine($"first: {lst.First.Value}");
+            Console.WriteLine($"last: {lst.Last.Value}");
 
-            //Console.WriteLine($"first: {lst.First.Value}");
-            //Console.WriteLine($"last: {lst.Last.Value}");
+            Console.WriteLine("\n===================================\n");
 
-            //List<int> list = new List<int>();
-            //list.Add(8);
-            //list.Add(9);
-            //list.Add(10);
-            //Console.WriteLine(string.Join(" ", list));
-            //list.RemoveAt(list.Count - 1);
-            //Console.WriteLine(string.Join(" ", list));
-            //int x = list[0];
-            //Console.WriteLine(x);
-
-            LinkedList lst1 = new LinkedList();
+            LinkedList lst1 = new();
             lst1.Append(90);
             lst1.Append(10);
             lst1.Append(3);
@@ -45,15 +35,12 @@ namespace ProjectOOP
             Console.WriteLine(string.Join(" ", lst1.ToList()));
             Console.WriteLine($"max: {lst1.GetMaxNode().Value}\n" +
                 $"min: {lst1.GetMinNode().Value}\n");
-            
+
             lst1.Unqueue();
             Console.WriteLine(string.Join(" ", lst1.ToList()));
             Console.WriteLine($"max: {lst1.GetMaxNode().Value}\n" +
                 $"min: {lst1.GetMinNode().Value}\n");
-
-
-
-
         }
     }
 }
+S
